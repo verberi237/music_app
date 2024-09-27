12025575d4cf8175e02806c8f7ff1368bd2c2284
unknown <limmyverberi@gmail.com>
2024-09-27 11:59:51 +0100

    i have created thr html template

---
+++ my mucic player/music.js
@@ -0,0 +1,20 @@
+const audio= document.getElementsByClassName(".audio")
+const playbtn= document.getElementsByClassName(".play")
+const prevbtn= document.getElementsByClassName(".prev")
+const nextbtn= document.getElementsByClassName(".next")
+const artist= document.getElementsByClassName(".artist")
+const name= document.getElementsByClassName(".lyrics")
+
+const.songs=[
+    {
+      title:"Bandana by Fireboy"
+      artist:Fireboy_ft_Asake,
+      src:" songs/song3.png",  
+    },
+    {
+        title:"under the influence"
+        artist:Chris_Brown,
+        src:"songs/song2.png",
+    },
+    
+]
